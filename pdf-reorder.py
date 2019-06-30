a492e1513b77e61bd3e02d209167e40cec2117ee
Paul Wilson <pwilson802@gmail.com>
2019-06-30 22:26:30 +1000

    working with cli

--- pdf-reorder.py
+++ pdf-reorder.py
@@ -17,10 +17,10 @@ pdf_files = os.listdir(folder)
 pdf_file_check = re.compile(r'.+[.]pdf')
 for file in pdf_files:
     try:
-        if not re.match(file_check, file).group == file:
+        if not pdf_file_check.match(file).group() == file:
             print('Check the files in the folder, one of them is not a pdf')
     except:
-        print('Check the files in the folder, one of them is not a pdf')
+        print('Exception - Check the files in the folder, one of them is not a pdf')
 
 # Making a dict of all the pdf files so they can be accessed easily later
 pdf_objects = []
@@ -38,48 +38,40 @@ for num,filename in enumerate(pdf_files):
 print(pdf_objects)
 print(f'{len(pdf_files)} PDF files are loaded, there are in this order:')
 for x in pdf_objects:
-    print(f"  {x['index']}: {x['filename']}") 
-
+    print(f"  {x['index']}: {x['filename']}  Pages: {x['pages']}") 
 
 
 print(f'Please indicate what order the new pdf should be in:')
 print('example: 1:1, 2:1, 1:1, 2:2, 1:1, 3:3')
 input_test = re.compile(r'[\d:\d, ]+')
+
+# Adding all the below to try/except loop to catch if an incorrect page number has been added
 while True:
-    page_layout = input('Enter the Pages: ')
-    if not re.match(input_test, page_layout).group() == page_layout:
-        print('Please enter the pages as valid input')
-        print('example: 1:1, 2:1, 1:1, 2:2, 1:1')
-    else:
-        print('Good, the new pdf will now be created.')
+    try:
+        while True:
+            page_layout = input('Enter the Pages: ')
+            if not re.match(input_test, page_layout).group() == page_layout:
+                print('Please enter the pages as valid input')
+                print('example: 1:1, 2:1, 1:1, 2:2, 1:1')
+            else:
+                print('Good, the new pdf will now be created.')
+                break
+
+        pdf_writer = PyPDF2.PdfFileWriter()
+        output = open('c:\\temp\\pdf\\out.pdf', 'wb')
+
+        for page_index in page_layout.split(','):
+            pdf, page = page_index.split(':')
+            pdf_path = [x for x in pdf_objects if x['index'] == pdf][0]['fullpath']
+            print(pdf_path)
+            print(page)
+            pdf_reader = PyPDF2.PdfFileReader(pdf_path)
+            pdf_writer.addPage(pdf_reader.getPage(int(page)-1))
         break
-
-pdfWriter = PyPDF2.PdfFileWriter()
-for page_index in page_layout.split(','):
-    pdf, page = page_index.split(':')
-    # print(pdf_objects)
-    # print([x for x in pdf_objects if x['index'] == pdf])
-    pdf_path = [x for x in pdf_objects if x['index'] == pdf][0]['fullpath']
-    with open(pdf_path, 'rb') as pdffile:
-        pdfreader = PyPDF2.PdfFileReader(pdffile)
-        pageObj = pdfreader.getPage(int(page))
-        pdfWriter.addPage(pageObj)
+    except IndexError as e: 
+        print(e)
+        print('An incorrect page number has been entered.  Please try again.')
 
 
-pdfOutputFile = open('c:\\temp\\pdf\\out.pdf', 'wb')
-pdfWriter.write(pdfOutputFile)
-pdfOutputFile.close()
-
-
-"""
-for pageNum in range(pdf1Reader.numPages):
-    pageObj = pdf1Reader.getPage(pageNum)
-    pdfWriter.addPage(pageObj)
-    pdfWriter.addPage(singleinsert)
-
-pdfOutputFile = open('c:\\temp\\cards\\dinofull.pdf', 'wb')
-pdfWriter.write(pdfOutputFile)
-pdfOutputFile.close()
-pdf1file.close()
-pdf2file.close()
-"""
+pdf_writer.write(output)
+output.close()
